032d52c62728005569265633fc793a36980d99ec
boburjonurazbayev <bobur7850@gmail.com>
2022-08-03 18:34:06 +0500

    Add socket

--- src/server.js
+++ src/server.js
@@ -3,9 +3,15 @@ import cors from "cors";
 import path from "path";
 import router from "./modules/index.js";
 import fileUpload from "express-fileupload";
+import {Server} from "socket.io";
+import http from "http"
+
+
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+
 
 app.use(cors());
 app.use(fileUpload({
@@ -15,4 +21,21 @@ app.use(express.json());
 app.use(express.static((path.join(process.cwd(), "src", "uploads"))));
 app.use(router);
 
-app.listen(PORT, () => console.log(`server running http://localhost:${PORT}`));
+
+
+const server = http.createServer(app);
+const io = new Server(server)
+
+  io.on("connection", (client) => {
+    client.on("new post", () => {
+        client.broadcast.emit("render new post");
+    });
+
+    client.on("activate post", (data) => {
+        client.broadcast.emit("render post", data);
+    });
+
+  });
+
+server.listen(PORT, () => console.log(`server running http://localhost:${PORT}`));
+
